3c66a036e798f5bb2f8635bf6e30c2a0a44a095a
Smartoneinok <Alexander_Novotny@aol.com>
2023-04-20 18:30:27 -0500

    rm

--- src/config.py
+++
@@ -1,5 +0,0 @@
-# BOX CONFIG - Check https://github.com/box-community/jwt-app-primer for help
-client_id = 'boptgksiplaa5dk3vug9dlyrw7qr99t5'
-client_secret = 'WlZsSyc3NYSzyPq4c386baFmgkAS0Gjo'
-enterprise_id = '273262935'  # IU's Dev Enterprise ID; yours may differ.
-metadata_template_name = 'Video Metadata'
